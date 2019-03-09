a3db62ded3d6f24c8447856a8eee070c4015b6e5
jw7zhang <47198312+jw7zhang@users.noreply.github.com>
2019-03-08 20:23:35 -0500

    Update inclusion.js

--- js/inclusion.js
+++ js/inclusion.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="`+SCRIPT_ROOT+`pages/about.html">About</a>
+                    <a class="nav-link" href="`+SCRIPT_ROOT+`pages/about2.html">About</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="`+SCRIPT_ROOT+`pages/newsfeed.html">News Feed</a>
